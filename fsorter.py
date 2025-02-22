223159bb3432034c2592a296ffb595e92cb04949
Ery <ery.cxtr@gmail.com>
2025-02-22 02:56:39 -0300

    Folder input

--- fsorter.py
+++ fsorter.py
@@ -1,59 +1,67 @@
 import cv2
 import numpy as np
 import os
+import shutil
 
-# Loading the image
-image = cv2.imread(r"C:\Users\Erica\Documents\Ery\4 SEMESTRE\undefined\WhatsApp Image 2024-12-12 at 18.41.42.jpeg")
+# Defining input folder
+input_folder = r"C:\Users\Erica\Documents\Ery\sorterfolders\undefined"
 
 # Defining the folders for the images
-red_folder = r"C:\Users\Erica\Documents\Ery\4 SEMESTRE\algebra"
-blue_folder = r"C:\Users\Erica\Documents\Ery\4 SEMESTRE\PAA"
+red_folder = r"C:\Users\Erica\Documents\Ery\sorterfolders\red"
+blue_folder = r"C:\Users\Erica\Documents\Ery\sorterfolders\blue"
 
-# Checking if the image was loaded sucessfully
-if image is None:
-    print("Error: Image not found, try another path!")
-else:
-    print("Image loaded successfully!")
+# Processing and moving the images
+def process_images(input_folder, red_folder, blue_folder):
+    # Verifying if the directories exist and creating them if they don't
+    os.makedirs(red_folder, exist_ok=True)
+    os.makedirs(blue_folder, exist_ok=True)
     
-    # Converting the image to HSV
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    # Iterating over the images in the input folder
+    for image_name in os.listdir(input_folder):
+        image_path = os.path.join(input_folder, image_name)
 
-    # Defining the range of red and blue color in HSV
-    lower_red = np.array([0, 50, 50])
-    upper_red = np.array([10, 255, 255])
-    mask_red = cv2.inRange(hsv, lower_red, upper_red)
+        # Verifying if the file is an image
+        if image_name.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
+            # Loading the image
+            image = cv2.imread(image_path)
 
-    lower_blue = np.array([110, 50, 50])
-    upper_blue = np.array([130, 255, 255])
-    mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
+            if image is None:
+                print("Error loading image ", image_name, ". Moving to next image.")
+                continue
 
-    # Counting the number of pixels in each color
-    red_pixels = np.sum(mask_red > 0)
-    blue_pixels = np.sum(mask_blue > 0)
+            # Converting the image to HSV
+            hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    # Deciding which folder to save the image
-    if red_pixels > blue_pixels:
-        folder = red_folder
-    elif blue_pixels > red_pixels:
-        folder = blue_folder
-    else:
-        folder = None
+            # Defining the range of red and blue color in HSV
+            lower_red = np.array([0, 50, 50])
+            upper_red = np.array([10, 255, 255])
+            mask_red = cv2.inRange(hsv, lower_red, upper_red)
 
-    #Displaying the results
-    print("Number of red pixels: ", red_pixels)
-    print("Number of blue pixels: ", blue_pixels)
+            lower_blue = np.array([110, 50, 50])
+            upper_blue = np.array([130, 255, 255])
+            mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
+
+            # Counting the number of pixels in each color
+            red_pixels = np.sum(mask_red > 0)
+            blue_pixels = np.sum(mask_blue > 0)
+
+            # Deciding which folder to save the image
+            if red_pixels > blue_pixels:
+                destination_folder = red_folder
+                print(f"{image_name} classified as red")
+            elif blue_pixels > red_pixels:
+                destination_folder = blue_folder
+                print(f"{image_name} classified as blue")
+            else:
+                print(f"No predominant color in image {image_name}. Moving to next image.")
+                continue
+
+            # Defining the destination path
+            destination_path = os.path.join(destination_folder, image_name)
+
+            # Moving the image
+            shutil.move(image_path, destination_path)
+            print(f"{image_name} moved to {destination_folder}!\n")
 
 
-    # Moving the image to the folder
-    if folder:
-        imwrite = cv2.imwrite(os.path.join(folder, "image.jpeg"), image)
-        print("Image sucessfully moved to ", folder, "!")
-    else:
-        print("No color was predominant in the image!")
-
-
-
-    # Displaying the image
-    cv2.imshow("Image", image)
-    cv2.waitKey(0) 
-    cv2.destroyAllWindows()
+process_images(input_folder, red_folder, blue_folder) 
